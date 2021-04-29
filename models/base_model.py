1729fc6e5b44b5d073636b89eac6169d3212783b
Daniel <dob9601@gmail.com>
2021-04-29 11:04:01 +0100

    Abstract base class stuff

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,31 @@
-from __future__ import annotations
+import abc
+from typing import Type, TypeVar
 
+from torch import nn
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.tensor import Tensor
 import torch.utils.data
 from torch.utils.data.dataset import Dataset
 
+V = TypeVar('V', bound='BaseModel')
 
-class CIFARModel(nn.Module):
 
-    # Set to -1 to use automatic number of epochs
-    epochs = -1
+class BaseModelMeta(abc.ABCMeta, type(nn.Module)):
+    """Metaclass that combines nn.Module with ABCMeta."""
+    ...
 
-    batch_size = 64
+
+class BaseModel(nn.Module, abc.ABC, metaclass=BaseModelMeta):
+
+    @property
+    def batch_size(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def epochs(self) -> int:
+        raise NotImplementedError()
 
     def __init__(self, dataset: Dataset, testset: Dataset):
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(3, 18, 5),  # Reduces size by 4px  (28px)
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),   # Halves image size    (14px)
-            nn.Conv2d(18, 64, 5),  # Reduces size by 4px  (10px)
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),   # Halves image size    (5px)
-        )
-
-        self.linear = nn.Sequential(
-            nn.Linear(64 * 5 * 5, 120),
-            nn.Linear(120, 60),
-            nn.Linear(60, 10)
-        )
 
         self.dataset = dataset
         self.dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size,
@@ -42,46 +36,6 @@ class CIFARModel(nn.Module):
                                                       shuffle=False, num_workers=2)
 
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        self.loss_function = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(self.parameters(), momentum=0.9, lr=0.001)
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.conv(x)
-        x = x.view(-1, 64 * 5 * 5)  # -1 means infer this dimension
-        x = self.linear(x)
-        return x
-
-    def train(self) -> float:
-        """
-        Run a singular epoch on the given dataset.
-
-        :param loader: The dataloader to load the data from.
-
-        :returns: The average loss across this epoch.
-        """
-        data_count = len(self.dataloader)
-
-        average_loss = 0
-        for i, data in enumerate(self.dataloader):
-            inputs: Tensor = data[0].to(self.device)
-            labels: Tensor = data[1].to(self.device)
-
-            self.optimizer.zero_grad()
-            outputs = self(inputs)
-            loss = self.loss_function(outputs, labels)
-            loss.backward()
-            self.optimizer.step()
-
-            average_loss += loss.item()
-
-            if i % 100 == 0:
-                # Only print every 100 batches so that printing doesn't
-                # slow down the model
-                print(f'Batch #{i}/{data_count}, Loss: {loss.item()}           ', end='\r')
-
-        print()
-        return average_loss / data_count
-
 
     def run(self):
         """
@@ -114,6 +68,7 @@ class CIFARModel(nn.Module):
                 epoch += 1
 
             print('Terminating due to 5 successive scores below best value')
+            return
 
 
         for epoch in range(self.epochs):
@@ -146,9 +101,19 @@ class CIFARModel(nn.Module):
 
         return correct / total
 
+    @abc.abstractmethod
+    def train(self) -> float:
+        """
+        Run a singular epoch on the given dataset.
+
+        :param loader: The dataloader to load the data from.
+
+        :returns: The average loss across this epoch.
+        """
+        ...
 
     @classmethod
-    def load(cls, path: str) -> CIFARModel:
+    def load(cls: Type[V], path: str) -> V:
         """
         Load the model after it has been serialised to a file.
 
---
+++ models/model_dob9601.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+from models.base_model import BaseModel
+
+import torch.nn as nn
+import torch.optim as optim
+from torch.tensor import Tensor
+from torch.utils.data.dataset import Dataset
+
+
+class CIFARModel(BaseModel):
+
+    # Set to -1 to use automatic number of epochs
+    epochs = -1
+
+    batch_size = 32
+
+    def __init__(self, dataset: Dataset, testset: Dataset):
+        super().__init__(dataset, testset)
+        self.conv = nn.Sequential(
+            nn.BatchNorm2d(3),
+            nn.Conv2d(3, 64, 3, padding=2),  # 32px
+            nn.ReLU(),
+            nn.MaxPool2d(4, 2),   # Halves image size    (16px)
+            nn.Conv2d(64, 128, 3, padding=2),  # 16px
+            nn.ReLU(),
+            nn.MaxPool2d(3, 2),   # 8px
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(128 * 8 * 8, 1024),
+            nn.Linear(1024, 512),
+            nn.Linear(512, 10)
+        )
+
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(self.parameters(), momentum=0.9, lr=0.001)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = x.view(-1, 128 * 8 * 8)  # -1 means infer this dimension
+        x = self.linear(x)
+        return x
+
+    def train(self) -> float:
+        data_count = len(self.dataloader)
+
+        average_loss = 0
+        for i, data in enumerate(self.dataloader):
+            inputs: Tensor = data[0].to(self.device)
+            labels: Tensor = data[1].to(self.device)
+
+            self.optimizer.zero_grad()
+            outputs = self(inputs)
+            loss = self.loss_function(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+
+            average_loss += loss.item()
+
+            if i % 100 == 0:
+                # Only print every 100 batches so that printing doesn't
+                # slow down the model
+                print(f'Batch #{i}/{data_count}, Loss: {loss.item()}           ', end='\r')
+
+        print()
+        return average_loss / data_count
+
