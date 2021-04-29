32dbadb9797beccbd3764362242d7b336fc13c45
Omar Omar <omarhatem5555@gmail.com>
2021-04-29 13:36:53 +0200

    Model Reformat

--- models/vgg_like_model.py
+++ models/vgg_like_model.py
@@ -1,40 +1,15 @@
 from __future__ import annotations
-import torchvision
-from torchvision import transforms
-import torch
+from base_model import BaseModel
+
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 from torch.tensor import Tensor
-import torch.utils.data
-from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Dataset
-
-
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-train_transform = transforms.Compose(
-    [  # transforms.RandomRotation(30),
-        # transforms.RandomResizedCrop(224),
-        # transforms.RandomHorizontalFlip(),
-        #  transforms.ColorJitter(brightness=0.4, contrast=0.4,
-        #                         saturation=0.4, hue=0.1),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-
-trainset = torchvision.datasets.CIFAR10(root='./datasets', train=True,
-                                        download=True, transform=train_transform)
-print(trainset)
-testset = torchvision.datasets.CIFAR10(root='./datasets', train=False,
-                                       download=True, transform=transform)
 
 
 class CIFARModel(nn.Module):
 
-    epochs = 100
+    epochs = -1
     batch_size = 64
 
     def __init__(self, dataset: Dataset, testset: Dataset):
@@ -77,35 +52,17 @@ class CIFARModel(nn.Module):
             nn.Linear(64, 10)
         )
 
-        self.dataset = dataset
-        self.dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size,
-                                                      shuffle=True, num_workers=2)
-
-        self.testset = testset
-        self.testloader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size,
-                                                      shuffle=False, num_workers=2)
-
-        self.device = torch.device(
-            'cuda:0' if torch.cuda.is_available() else 'cpu')
         self.loss_function = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(self.parameters(), momentum=0.9, lr=0.004)
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.conv(x)
-        # print(x.shape)
         x = x.view(-1, 256 * 3 * 3)
         x = self.linear(x)
 
         return x
 
     def train(self) -> float:
-        """
-        Run a singular epoch on the given dataset.
-
-        :param loader: The dataloader to load the data from.
-
-        :returns: The average loss across this epoch.
-        """
         data_count = len(self.dataloader)
 
         average_loss = 0
@@ -129,66 +86,3 @@ class CIFARModel(nn.Module):
 
         print()
         return average_loss / data_count
-
-    def run(self):
-        """
-        Run the model on a given dataset.
-
-        :param dataset: The dataset to run on
-        """
-
-        for epoch in range(self.epochs):
-            print(f'Epoch #{epoch} ')
-
-            average_loss = self.train()
-            print(f'[DONE] [Average Loss: {average_loss}]')
-            print(self.calculate_accuracy())
-
-    def calculate_accuracy(self) -> float:
-        """
-        Calculate the accuracy of the model by testing it on its dataset.
-
-        :returns: A float representing the accuracy of the model.
-        """
-        correct = 0
-        total = 0
-
-        with torch.no_grad():
-            for data in self.testloader:
-                images, labels = data[0].to(
-                    self.device), data[1].to(self.device)
-                outputs = self(images)
-
-                _, predicted = torch.max(outputs.data, 1)
-
-                # The number of items is the 0th dimension
-                total += labels.size(0)
-
-                # The sum of a boolean tensor is equal to the number of "Trues" in it
-                correct += (predicted == labels).sum().item()
-
-        return correct / total
-
-    @classmethod
-    def load(cls, path: str) -> CIFARModel:
-        """
-        Load the model after it has been serialised to a file.
-
-        :param path: The path of the file.
-
-        :returns: An instance of CIFARModel, constructed from the file.
-        """
-        return torch.load(path)
-
-    def save(self, path: str) -> None:
-        """
-        Save the model to a file.
-
-        :param path: The path to save to.
-        """
-        torch.save(self, path)
-
-
-model = CIFARModel(trainset, testset).to('cuda')
-model.run()
-print(model.calculate_accuracy())
