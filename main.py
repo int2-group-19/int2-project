3791dc56388fd64de4b74e8771e3584013a76319
Daniel <dob9601@gmail.com>
2021-05-01 16:28:13 +0100

    Main guard

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import torchvision
 from torchvision import transforms
-from models.model_dob9601 import CIFARModel
+from models.vgg_v2 import CIFARModel
 
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def main():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./datasets', train=True,
-                                        download=True, transform=transform)
-testset = torchvision.datasets.CIFAR10(root='./datasets', train=False,
-                                       download=True, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root='./datasets', train=True,
+                                            download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root='./datasets', train=False,
+                                           download=True, transform=transform)
 
-model = CIFARModel(trainset, testset).to('cuda')
-model.run()
+    model = CIFARModel(trainset, testset).to('cuda')
+    model.run()
+
+if __name__ == '__main__':
+    main()
